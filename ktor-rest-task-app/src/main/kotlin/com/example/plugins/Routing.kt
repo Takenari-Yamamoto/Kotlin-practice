fef005a7a4681f02d0a296a00735bef6a86161ee
Takenari Yamamoto <tktkymnr1216@gmail.com>
2024-10-15 12:19:14 +0900

    init rest api app

--- ktor-rest-task-app/src/main/kotlin/com/example/plugins/Routing.kt
+++ ktor-rest-task-app/src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,6 +1,8 @@
 package com.example.plugins
 
+import com.example.model.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
